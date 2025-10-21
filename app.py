d5df95a09dddf386bfd3e90dbf0a4aa6a9217d9b
Aman Kumar <aman.akumar.choudhary>
2025-10-21 09:16:29 +0530

    prepare for deployment

--- app.py
+++ app.py
@@ -243,4 +243,5 @@ def api_weather(city):
     return jsonify({'error': 'City not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
